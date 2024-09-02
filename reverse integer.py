830ecfe801b31b4c3c435b6581fa330ab4949990
Rupithkumar <133633120+Rupithkumar@users.noreply.github.com>
2024-09-02 08:29:42 +0530

    update

--- reverse integer.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        if x<0:
-            a=int(str(x)[1:][::-1])*-1
-        else:
-            a=int(str(x)[::-1])
-        if a>2**31-1 or a <-2**31:
-            return 0
-        return a
